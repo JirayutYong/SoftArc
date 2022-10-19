41c5f886a201ba627d18de2de5a8bff7f2d26757
x_Yong_x <98368414+JirayutYong@users.noreply.github.com>
2022-10-19 16:35:57 +0700

    Delete SOLID Principles directory

--- SOLID Principles/lsp/src/Shape.java
+++
@@ -1,9 +0,0 @@
-public interface Shape {
-  public int getArea();
-  public void setWidth(int width);
-  public void setHeight(int height);
-  public int getWidth();
-  public int getHeight();
-}
-
-
